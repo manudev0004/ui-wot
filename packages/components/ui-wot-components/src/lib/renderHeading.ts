22eab4e3b7ba5c1c77cb58cfd3b022114dbd4e34
Manu Dev <manudev0004@gmail.com>
2025-06-24 21:04:26 +0530

    some changes

--- packages/components/ui-wot-components/src/lib/renderHeading.ts
+++ packages/components/ui-wot-components/src/lib/renderHeading.ts
@@ -1,18 +1,9 @@
 import '../components/ui-heading/ui-heading';
 
 
-export function renderHeading(text: string, container: HTMLElement) {
-  console.log('renderHeading called with:', text, container);
-  
-  // Check if custom element is defined
-  console.log('ui-heading defined?', customElements.get('ui-heading'));
-  
-  const el = document.createElement('ui-heading');
-  console.log('Created element:', el);
-  
-  el.setAttribute('text', text);
-  console.log('Set text attribute, element:', el);
-  
+export function renderHeading(text: string, container: HTMLElement) {  
+  const el = document.createElement('ui-heading');  
+  el.setAttribute('text', text);  
   container.innerHTML = '';
   container.appendChild(el);
   
