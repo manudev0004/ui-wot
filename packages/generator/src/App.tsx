88c25e5ab8e57d31f6f9d1f5ae780bd68d70bbae
Manu Dev <manudev0004@gmail.com>
2025-06-24 21:23:20 +0530

    small fixes

--- packages/generator/src/App.tsx
+++ packages/generator/src/App.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useRef } from "react";
 import { renderHeading } from "ui-wot-components";
+import { defineCustomElements } from "ui-wot-components/loader";
 
 function App() {
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
+    defineCustomElements();
     if (containerRef.current) {
       renderHeading("Hello from Stencil + React!", containerRef.current);
     }
