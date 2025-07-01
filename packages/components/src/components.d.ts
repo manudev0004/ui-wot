7a75518689e2444a227c85c7a9c015871c524145
Manu Dev <manudev0004@gmail.com>
2025-07-01 13:08:54 +0530

    Removed default example and licence

--- packages/components/src/components.d.ts
+++ packages/components/src/components.d.ts
@@ -6,31 +6,11 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first": string;
-        /**
-          * The last name
-         */
-        "last": string;
-        /**
-          * The middle name
-         */
-        "middle": string;
-    }
     interface UiHeading {
         "text": string;
     }
 }
 declare global {
-    interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
-    }
-    var HTMLMyComponentElement: {
-        prototype: HTMLMyComponentElement;
-        new (): HTMLMyComponentElement;
-    };
     interface HTMLUiHeadingElement extends Components.UiHeading, HTMLStencilElement {
     }
     var HTMLUiHeadingElement: {
@@ -38,30 +18,14 @@ declare global {
         new (): HTMLUiHeadingElement;
     };
     interface HTMLElementTagNameMap {
-        "my-component": HTMLMyComponentElement;
         "ui-heading": HTMLUiHeadingElement;
     }
 }
 declare namespace LocalJSX {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first"?: string;
-        /**
-          * The last name
-         */
-        "last"?: string;
-        /**
-          * The middle name
-         */
-        "middle"?: string;
-    }
     interface UiHeading {
         "text"?: string;
     }
     interface IntrinsicElements {
-        "my-component": MyComponent;
         "ui-heading": UiHeading;
     }
 }
@@ -69,7 +33,6 @@ export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
-            "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
             "ui-heading": LocalJSX.UiHeading & JSXBase.HTMLAttributes<HTMLUiHeadingElement>;
         }
     }
