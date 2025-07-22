5909b7556a254f11b4750940ba5ff78a5459b33c
Manu Dev <manudev0004@gmail.com>
2025-07-22 19:23:30 +0530

    chore: update gitignore file and package and other files to add tailwind support

---
+++ packages/components/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{ts,tsx,html}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#067362',
+        secondary: '#B84A91',
+      }
+    },
+  },
+  plugins: [],
+}
