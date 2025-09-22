9d97f7f9a0794111e5403af8ec57d7078a62c3c9
Manu Dev <manudev0004@gmail.com>
2025-09-22 06:32:17 +0530

    feat: component library with new services and utilities

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,11 +1,40 @@
 /**
- * @fileoverview entry point for your component library
+ * @fileoverview Simple entry point for UI-WoT components
  *
- * This is the entry point for your component library. Use this file to export utilities,
- * constants or data structure that accompany your components.
- *
- * DO NOT use this file to export your components. Instead, use the recommended approaches
- * to consume components of this package as outlined in the `README.md`.
+ * This exports the component classes for use in frameworks.
+ * For HTML usage, just import the built bundle directly.
  */
 
-export type * from './components';
+// Export components for framework usage
+export { UiButton } from './components/ui-button/ui-button';
+export { UiToggle } from './components/ui-toggle/ui-toggle';
+export { UiSlider } from './components/ui-slider/ui-slider';
+export { UiNumberPicker } from './components/ui-number-picker/ui-number-picker';
+export { UiCheckbox } from './components/ui-checkbox/ui-checkbox';
+export { UiCalendar } from './components/ui-calendar/ui-calendar';
+export { UiNotification } from './components/ui-notification/ui-notification';
+export { UiText } from './components/ui-text/ui-text';
+export { UiEvent } from './components/ui-event/ui-event';
+export { UiColorPicker } from './components/ui-color-picker/ui-color-picker';
+export { UiFilePicker } from './components/ui-file-picker/ui-file-picker';
+export { UiObject } from './components/ui-object/ui-object';
+
+// Export utility types
+export * from './utils/types';
+
+// Export services (tree-shakable)
+export * as Services from './services';
+
+// Also export browser-client helpers as top-level named exports for convenience
+
+// (Auto-connect helpers omitted in this build)
+
+// Browser bundle helpers (dynamic WoT loader + one-liners)
+export {
+  initializeWot,
+  initiliseWot,
+  connectProperty as connectProperty,
+  connectAction as connectAction,
+  connectEvent as connectEvent,
+  connectAll as connectAll,
+} from './services/browser-bundle-connect';
